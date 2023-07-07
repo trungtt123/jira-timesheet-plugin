cad8d840abc823939be3ef8b6e43012d1e91b32f
MF Trung.tq <trung.tq@mediafusion.co.jp>
2023-07-07 09:55:50 +0700

    remove function

--- src/js/kintone.service.js
+++ src/js/kintone.service.js
@@ -1,37 +1,3 @@
-async function updateRecord(body) {
-    return new Promise((resolve, reject) => {
-        kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', body, function (resp) {
-            // Xử lý thành công
-            resolve(resp);
-        }, function (error) {
-            // Xử lý lỗi
-            reject(error);
-        });
-    });
-}
-async function uploadFile(formData) {
-    const domainKintone = window.location.hostname;
-    let url = `https://${domainKintone}/k/v1/file.json`;
-    try {
-        let response = await fetch(url, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-        });
-        if (response.ok) {
-            return await response.json();
-        } else {
-            let errorData = await response.json();
-            console.log(errorData);
-            // Xử lý lỗi
-        }
-    } catch (error) {
-        console.log(error);
-        // Xử lý lỗi
-    }
-}
 async function addRecord(body) {
     return new Promise((resolve, reject) => {
         kintone.api(kintone.api.url('/k/v1/record', true), 'POST', body, function (resp) {
@@ -135,33 +101,6 @@ async function getAllRecordsFromKintone(appId) {
     }
 }
 
-async function downloadAndReadKintoneFile(fileKey) {
-    return new Promise((resolve, reject) => {
-        const domainKintone = window.location.hostname;
-        let url = `https://${domainKintone}/k/v1/file.json?fileKey=${fileKey}`;
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.responseType = 'blob';
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                let blob = xhr.response;
-                let reader = new FileReader();
-                reader.onloadend = function () {
-                    let fileData = reader.result;
-                    resolve(fileData);
-                };
-                reader.onerror = function (error) {
-                    reject(error);
-                };
-                reader.readAsText(blob);
-            } else {
-                reject(xhr.responseText);
-            }
-        };
-        xhr.send();
-    });
-}
 async function proxyRequest(PLUGIN_ID, apiUrl, method, body, headers) {
     return new Promise((resolve, reject) => {
         kintone.plugin.app.proxy(PLUGIN_ID, apiUrl, method, body, headers, function (respBody, respStatus, respHeaders) {
